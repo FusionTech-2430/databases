d670906b157cefb32f196e13a6f42f1328d13a32
Estebans441 <salazaraesteban@javeriana.edu.co>
2024-09-30 12:12:02 -0500

    ♻️  project structure

--- mysql/scripts/00_master_config.sql
+++
@@ -1,6 +0,0 @@
-ALTER USER 'replication_user'@'%' IDENTIFIED WITH 'mysql_native_password' BY 'rep_pass';
-GRANT REPLICATION SLAVE ON *.* TO 'replication_user'@'%';
-FLUSH PRIVILEGES;
-SHOW MASTER STATUS;
-# mysql-bin.000003
-# 850
